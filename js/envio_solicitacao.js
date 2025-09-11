cfecfcf3ce3c5b6973e026767360fe238410d899
GULTEMBERGUE CARLOS REGO <gutoapollo@hotmail.com>
2025-09-11 17:58:10 -0300

    3

--- js/envio_solicitacao.js
+++ js/envio_solicitacao.js
@@ -308,6 +308,7 @@ async function gerarPDF() {
     doc.setFont('helvetica', 'normal');
     doc.text(enviadoPor, currentX, startY);
 
+    
     // Adicionar QTD Equipe em vermelho
     doc.setTextColor('#f44336'); // Cor vermelha
     drawLabeledText('QTD Equipe: ', solicitacao.veiculo?.qtd_equipe || 'N/A', leftMargin, startY);
