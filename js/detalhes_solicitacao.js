ebf7ad3ae3fb8d2c0afd39d0a677893d9dec14f5
GULTEMBERGUE CARLOS REGO <gutoapollo@hotmail.com>
2025-09-26 14:50:27 -0300

    1

--- js/detalhes_solicitacao.js
+++ js/detalhes_solicitacao.js
@@ -292,7 +292,7 @@ async function gerarPDF() {
             break;
         case 'aprovado':
             tituloPDF = 'Pendente Envio de Peças';
-            tituloCor = '#2196F3'; // Azul
+            tituloCor = '#7221f3ff'; // Azul
             break;
     }
     // Título do Documento
